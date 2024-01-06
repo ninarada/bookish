19eb461b95797a9b6423bab824f9872621f829e7
ninarada <nradja00@fesb.hr>
2024-01-06 16:57:10 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,7 @@ const pages: Record<string, `/${string}`> = {
   profile: "/profile",
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children,}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={lora.className}>
