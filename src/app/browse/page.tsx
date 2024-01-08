12313ccc2fd4ea9f7059f5d81fe19661adfd7d1a
ninarada <nradja00@fesb.hr>
2024-01-08 19:12:04 +0100

    .

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -6,6 +6,9 @@ import getSubject from "../api/functions/getSubject";
 
 export default async function Browse () {
     const fiction: {keyWork: TypeWork, keyAuthor: TypeAuthor[]} [] = await getSubject('fiction', '6');
+    
+    const romance: {keyWork: TypeWork, keyAuthor: TypeAuthor[]} [] = await getSubject('romance', '6');
+
 
     return (
         <main>
@@ -13,6 +16,9 @@ export default async function Browse () {
             <div>
               <h2>Fiction</h2>
               <BookSlider subject={fiction}/>
+              <h2>Romance</h2>
+              <BookSlider subject={romance}/>
+
             </div>
             
         </main>
