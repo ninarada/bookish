e52f1bb290aa49c60510a879a4c73324889a95dc
ninarada <nradja00@fesb.hr>
2024-05-01 17:20:47 +0200

    .

--- src/app/works/[workId]/page.tsx
+++ src/app/works/[workId]/page.tsx
@@ -10,12 +10,12 @@ import RatingStars from "@/app/_components/ratingStars/RatingStars";
 import getSubject from "@/app/api/functions/getSubject";
 import BookSlider from "@/app/_components/books_slider/slider";
 
-function getRandomElementFromArray(array) {
+function getRandomElementFromArray(array: string | any[]) {
     const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
 }
 
-function convertToSnakeCase(text) {
+function convertToSnakeCase(text: string) {
     return text.toLowerCase().replace(/\s+/g, '_');
 }
 
@@ -24,18 +24,26 @@ interface Params {
 }
 
 export default async function BookPage({ params }: { params: Params }) {
-    const work: TypeWork = await getWork(params.workId); 
+    const work = await getWork(params.workId); 
+
+    if (work === null) {
+        return <div>Error: Book not found</div>;
+    }
+
     const authorKeys = work?.authors.map(i => {
         if(i.key!=undefined) {
             return extractKeyFromArray(i.key)
         } else {
             return ''
-        }
-        
+        } 
     });
-    const authors: TypeAuthor[] = work?.authors && await Promise.all(authorKeys.map(async (authorKey) => {
+
+    const nonfilteredAuthors = work?.authors && await Promise.all(authorKeys.map(async (authorKey) => {
         return await getAuthor(authorKey);
     })) 
+
+    const authors: TypeAuthor[] = nonfilteredAuthors.filter((author) => author !== null) as TypeAuthor[];
+
     const cover =  getCovers('id', String(work.covers[0]));
 
     const subjectSuggestion = work?.subjects || '';
